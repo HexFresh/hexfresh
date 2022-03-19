7ef0bffade5a75df4ef04b7ba9afd88205c1f0da
Hà Minh Cường <55825733+haminhcuong@users.noreply.github.com>
2022-03-19 21:31:56 +0700

    update selected task

--- src/components/mentor/task-content/TaskContent.tsx
+++ src/components/mentor/task-content/TaskContent.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './task-content.css';
 import MultipleChoiceTask from './type/MultipleChoiceTask';
 
-
 interface ITask {
   id: number;
   checklistId: number;
@@ -30,11 +29,7 @@ function TaskContent(props: any) {
   return (
     <div className="task-content">
       <div className="task-content-container">
-        <div className="top">
-          <div className="task-title">{task.title}</div>
-          <div className="task-point">{`Point: ${task.point}`}</div>
-          
-        </div>
+        <div className="top">{task.title}</div>
         <div className="bottom">{renderTask(task)}</div>
       </div>
     </div>
