a560e539781bfcf15039386a9d8261564b9c2d6d
Hà Minh Cường <55825733+haminhcuong@users.noreply.github.com>
2022-04-08 13:35:20 +0700

    Update socketioInit.ts

--- src/utils/socketioInit.ts
+++ src/utils/socketioInit.ts
@@ -1,19 +1,22 @@
-import io from "socket.io-client";
+import io from 'socket.io-client';
 
 export const initSocket = (accessToken: string) => {
-    const socket = io("http://localhost:3000", { transports: ['websocket'], query: { token: accessToken } },);
-    socket.on('ping', (data) => {
-        alert(data)
-    })
-    socket.on('refetch notification', (data) => {
-        const alertData = `
+  const socket = io('https://hexfresh-gamification-backend.herokuapp.com', {
+    transports: ['websocket'],
+    query: { token: accessToken },
+  });
+  socket.on('ping', (data) => {
+    alert(data);
+  });
+  socket.on('refetch notification', (data) => {
+    const alertData = `
             title: ${data[0].title}\n
             body: ${data[0].body}\n
             time: ${data[0].createdAt}
         `;
-        console.log(data);
-        alert(alertData);
-        // To do: implement refetch & update notification
-    })
-    return socket;
-}
+    console.log(data);
+    alert(alertData);
+    // To do: implement refetch & update notification
+  });
+  return socket;
+};
