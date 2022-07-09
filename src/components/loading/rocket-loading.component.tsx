ca420890002bec14e73bfb7d7116879cee3ba38f
HUYNH HO THANH TRA <huynhhothanhtra2000@gmail.com>
2022-07-10 04:17:56 +0700

    Add verify role to render resource

--- src/components/loading/rocket-loading.component.tsx
+++ src/components/loading/rocket-loading.component.tsx
@@ -1,8 +1,8 @@
 import { Typography } from "antd"
 
-export const RocketLoading = ()=>{
-  return <div className="loading" >
-  <img src="/gifrocket-rocket.gif" alt="loading rocket" />
-  <Typography.Text className="text">Please wait a second...</Typography.Text>
-</div>
+export const RocketLoading = ({ className = '' }: { className?: string }) => {
+  return <div className={`${className} rocket loading`} >
+    <img src="/gifrocket-rocket.gif" alt="loading rocket" />
+    <Typography.Text className="text">Please wait a second...</Typography.Text>
+  </div>
 }
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,6 +1,8 @@
 export const INT_ZERO = 0;
 export const INT_ONE = 1;
 export const INT_TWO = 2;
+export const INT_THREE = 3;
+export const INT_FOUR = 4;
 export const DEFAULT_PAGE_SIZE = 24;
 export enum USER_PROFILE_TABS{
   OVERVIEW = 'overview', //default tab
@@ -9,3 +11,13 @@ export enum USER_PROFILE_TABS{
 }
 
  export const AVATAR_SYSTEM_SRC = "https://res.cloudinary.com/droruloek/image/upload/v1656213477/hexfresh/hexfresh_r6lvaq.svg";
+
+ export const ROLE = Object.freeze({
+  MENTOR: 'mentor',
+  FRESHER: 'fresher',
+ })
+
+ export const USER_ROLE = Object.freeze({
+  [ROLE.MENTOR]: INT_THREE,
+  [ROLE.FRESHER]: INT_FOUR,
+ })
