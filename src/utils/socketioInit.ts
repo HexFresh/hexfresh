a0223026550ed849619efb2367fef82146e193f3
HUYNH HO THANH TRA <huynhhothanhtra2000@gmail.com>
2022-07-13 23:22:43 +0700

    reverse url for socket init ;)

--- src/utils/socketioInit.ts
+++ src/utils/socketioInit.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 export const initSocket = () => {
   const accessToken = localStorage.getItem('token');
-  const url =  process.env.REACT_APP_CHAT_SERVER_URL;
+  const url =  'https://hexfresh-socket.herokuapp.com';
   const socket = io(url as string, {
     transports: ['websocket'],
     query: { token: accessToken },
